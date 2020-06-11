0e33073c84930dc62cc84538c89fbbe92ce82f61
caojin <caojin94@hotmail.com>
2020-06-11 10:38:02 +0800

    test pull request

--- LogServer.h
+++ LogServer.h
@@ -1,32 +1,32 @@
-#ifndef LOG_SERVER_H
-#define LOG_SERVER_H
-#include "PrivateDefine.h"
+// #ifndef LOG_SERVER_H
+// #define LOG_SERVER_H
+// #include "PrivateDefine.h"
 
-#include <time.h>
-#include <string>
-#include <deque>
-#include <mutex>
+// #include <time.h>
+// #include <string>
+// #include <deque>
+// #include <mutex>
 
-class LogServer
-{
-private:
-	int m_logFPS = -1;
-	clock_t m_logTick = 0;
-	std::deque<std::tstring> m_dLogCache;
-	std::mutex m_mtxLogCache;
-	bool *m_exit = nullptr;
-	volatile bool m_closing = false;
+// class LogServer
+// {
+// private:
+// 	int m_logFPS = -1;
+// 	clock_t m_logTick = 0;
+// 	std::deque<std::tstring> m_dLogCache;
+// 	std::mutex m_mtxLogCache;
+// 	bool *m_exit = nullptr;
+// 	volatile bool m_closing = false;
 
-	void ShowLogThread();
-public:
-	LogServer();
-	LogServer(int logFPS);
-	~LogServer();
-	void ShowLogImmediate(tchar *tstr);
-	void ShowLog(tchar *tstr);
-	void ShowLogImmediate(std::tstring str);
-	void ShowLog(std::tstring str);
-};
+// 	void ShowLogThread();
+// public:
+// 	LogServer();
+// 	LogServer(int logFPS);
+// 	~LogServer();
+// 	void ShowLogImmediate(tchar *tstr);
+// 	void ShowLog(tchar *tstr);
+// 	void ShowLogImmediate(std::tstring str);
+// 	void ShowLog(std::tstring str);
+// };
 
-extern LogServer g_logServer;
-#endif
+// extern LogServer g_logServer;
+// #endif
